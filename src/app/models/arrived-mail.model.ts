f3745589e573e76f8e62399d9a3ecf0356a156bd
mzeyad <9eryetElvet7>
2019-10-26 23:11:17 +0200

    init mail receive form from frontend

---
+++ src/app/models/arrived-mail.model.ts
@@ -0,0 +1,15 @@
+
+export class ArrivedMailModelModel{
+    idEntry: string;
+    idDirectory: string;
+    subject: string;
+    sender: string;
+    receptionDate: string;
+    idReceivedDocument: string;
+    dateReceivedDocument: string;
+    priorityDegree: string;
+    attachments: string;
+    observations: string;
+}
+
+
