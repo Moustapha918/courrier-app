c07a6848265d4e4ddb62935d310b744db1745490
tah <babaahmedelmokhtar@gmail.com>
2019-10-27 01:02:25 +0200

    get all arrived mail from backend ant they in table

--- src/app/pages/arrived-mail-sc/arrived-mail-sc.component.ts
+++ src/app/pages/arrived-mail-sc/arrived-mail-sc.component.ts
@@ -5,33 +5,12 @@ import {takeUntil} from 'rxjs/operators';
 import {FuseUtils} from '../../../@fuse/utils';
 import {Subject} from 'rxjs';
 import {fuseAnimations} from '../../../@fuse/animations';
+import {ArrivedMailModelModel} from '../../models/arrived-mail.model';
+import {InitMailService} from '../../services/init-mail.service';
 
 
-export interface Mail {
-    id: string;
-    idEntry: number;
-    idDossier: number;
-    subject: string;
-    sender: number;
-    receptionDate: string;
-    idReceivedDocument: number;
-    dateReceivedDocument: string;
-    priorityDegree: number;
-    attachment: string;
-    observations: string;
-    direction: any;
-    service: any;
-    division: any;
-    step: string;
-
-}
 
 
-const ELEMENT_DATA: Mail[] = [
-/*    {idEntry: 1, subject: 'Hydrogen', sender: 1.0079, receptionDate: 'H'},
-    {idEntry: 2, subject: 'Helium', sender: 4.0026, receptionDate: 'He'},*/
-
-];
 
 
 @Component({
@@ -44,209 +23,31 @@ const ELEMENT_DATA: Mail[] = [
 export class ArrivedMailScComponent {
 
 
-    // dataSource: FilesDataSource | null;
+    arrivedMails: ArrivedMailModelModel[];
     displayedColumns: string[] = ['idEntry', 'subject', 'sender', 'receptionDate'];
-    dataSource = ELEMENT_DATA;
-  /*  @ViewChild(MatPaginator, {static: true})
-    paginator: MatPaginator;
 
-    @ViewChild(MatSort, {static: true})
-    sort: MatSort;
-
-    @ViewChild('filter', {static: true})
-    filter: ElementRef;*/
-
-    // Private
-    private _unsubscribeAll: Subject<any>;
-
-    constructor(
-       /* private _ecommerceProductsService: EcommerceProductsService*/
-    )
+    constructor(private initMailService: InitMailService)
     {
-        // Set the private defaults
-        this._unsubscribeAll = new Subject();
+
     }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
 
     /**
      * On init
      */
-/*    ngOnInit(): void
-    {
-        this.dataSource = new FilesDataSource(this._ecommerceProductsService, this.paginator, this.sort);
 
-        fromEvent(this.filter.nativeElement, 'keyup')
-            .pipe(
-                takeUntil(this._unsubscribeAll),
-                debounceTime(150),
-                distinctUntilChanged()
-            )
-            .subscribe(() => {
-                if ( !this.dataSource )
-                {
-                    return;
+    // tslint:disable-next-line:use-lifecycle-interface
+    ngOnInit(): void{
+        this.initMailService.getAllArrivedMailsFromBackend()
+            .subscribe(
+                (allArrivedMAils) => {
+                    this.arrivedMails = allArrivedMAils;
+                },
+                (error) => {
+                    console.log('Error ! : ' + error);
                 }
+            );
 
-                this.dataSource.filter = this.filter.nativeElement.value;
-            });
-    }*/
-}
-
-/*
-export class FilesDataSource extends DataSource<any>
-{
-    private _filterChange = new BehaviorSubject('');
-    private _filteredDataChange = new BehaviorSubject('');
-
-    /!**
-     * Constructor
-     *
-     * @param {EcommerceProductsService} _ecommerceProductsService
-     * @param {MatPaginator} _matPaginator
-     * @param {MatSort} _matSort
-     *!/
-    constructor(
-        private _ecommerceProductsService: EcommerceProductsService,
-        private _matPaginator: MatPaginator,
-        private _matSort: MatSort
-    )
-    {
-        super();
-
-        this.filteredData = this._ecommerceProductsService.products;
-    }
-
-    /!**
-     * Connect function called by the table to retrieve one stream containing the data to render.
-     *
-     * @returns {Observable<any[]>}
-     *!/
-    connect(): Observable<any[]>
-    {
-        const displayDataChanges = [
-            this._ecommerceProductsService.onProductsChanged,
-            this._matPaginator.page,
-            this._filterChange,
-            this._matSort.sortChange
-        ];
-
-        return merge(...displayDataChanges)
-            .pipe(
-                map(() => {
-                        let data = this._ecommerceProductsService.products.slice();
-
-                        data = this.filterData(data);
-
-                        this.filteredData = [...data];
-
-                        data = this.sortData(data);
-
-                        // Grab the page's slice of data.
-                        const startIndex = this._matPaginator.pageIndex * this._matPaginator.pageSize;
-                        return data.splice(startIndex, this._matPaginator.pageSize);
-                    }
-                ));
-    }
-
-    // -----------------------------------------------------------------------------------------------------
-    // @ Accessors
-    // -----------------------------------------------------------------------------------------------------
-
-    // Filtered data
-    get filteredData(): any
-    {
-        return this._filteredDataChange.value;
-    }
-
-    set filteredData(value: any)
-    {
-        this._filteredDataChange.next(value);
-    }
-
-    // Filter
-    get filter(): string
-    {
-        return this._filterChange.value;
-    }
-
-    set filter(filter: string)
-    {
-        this._filterChange.next(filter);
-    }
-
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
-
-    /!**
-     * Filter data
-     *
-     * @param data
-     * @returns {any}
-     *!/
-    filterData(data): any
-    {
-        if ( !this.filter )
-        {
-            return data;
-        }
-        return FuseUtils.filterArrayByString(data, this.filter);
-    }
-
-    /!**
-     * Sort data
-     *
-     * @param data
-     * @returns {any[]}
-     *!/
-    sortData(data): any[]
-    {
-        if ( !this._matSort.active || this._matSort.direction === '' )
-        {
-            return data;
-        }
-
-        return data.sort((a, b) => {
-            let propertyA: number | string = '';
-            let propertyB: number | string = '';
-
-            switch ( this._matSort.active )
-            {
-                case 'id':
-                    [propertyA, propertyB] = [a.id, b.id];
-                    break;
-                case 'name':
-                    [propertyA, propertyB] = [a.name, b.name];
-                    break;
-                case 'categories':
-                    [propertyA, propertyB] = [a.categories[0], b.categories[0]];
-                    break;
-                case 'price':
-                    [propertyA, propertyB] = [a.priceTaxIncl, b.priceTaxIncl];
-                    break;
-                case 'quantity':
-                    [propertyA, propertyB] = [a.quantity, b.quantity];
-                    break;
-                case 'active':
-                    [propertyA, propertyB] = [a.active, b.active];
-                    break;
-            }
-
-            const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
-            const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
-
-            return (valueA < valueB ? -1 : 1) * (this._matSort.direction === 'asc' ? 1 : -1);
-        });
-    }
-
-    /!**
-     * Disconnect
-     *!/
-    disconnect(): void
-    {
     }
 }
-*/
+
