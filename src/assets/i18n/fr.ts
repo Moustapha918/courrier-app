cdca30dae6fab950c9742711d49da4abbe00559b
tah <babaahmedelmokhtar@gmail.com>
2020-01-14 22:44:57 +0100

    translate done on the pages SC-Home, init-arrived-mail, arrived-mail-sc

--- src/assets/i18n/fr.ts
+++ src/assets/i18n/fr.ts
@@ -1,10 +1,35 @@
 export const locale = {
     lang: 'fr',
     data: {
-        Courrier: {
-            'Courrier Arrivé': 'Courrier Arrivé',
-            'Chercher un courrier': 'Chercher un courrier',
-            'Expediteur': 'Expediteur'
+        mail: {
+            COURRIER_ARRIVEE                : 'Courrier Arrivé',
+            COURRIER_DEPART                 : 'Courrier Depart',
+            ARCHIVE                         : 'Archive',
+            CHERCHER_COURRIER               : 'Chercher un courrier',
+            AJOUTER_COURRIER                : 'Ajouter un courrier',
+            NUMERO_ENTREE                   : 'N° d’entrée',
+            OBJET                           : 'Objet',
+            EXPEDITEUR                      : 'Expediteur',
+            DATE                            : 'Date de réception',
+            CREATE_MAIL                     : 'Créer un courrier arrivé',
+            MAIL_DATE                       : 'Date de reception du courrier',
+            DIRECTORY_ID                    : 'Numéro du dossier',
+            ID_ENTRY                        : 'Numéro d\'entré',
+            IDRECEIVED_DOCUMENT             : 'Réference du document réçu',
+            IDRECEIVED_DOCUMENT_MSG         : 'La référence est obligatoire!',
+            RECEIVED_DOCUMENT_DATE          : 'Date du document réçu',
+            SENDER_MAIL                     : 'Expéditeur du courrier',
+            SENDER_MAIL_MSG                 : 'L\'expediteur est obligatoire !',
+            SUBJECT_MAIL                    : 'Objet du courrier',
+            SUBJECT_MAIL_MSG                : 'L\'objet est obligatoire !',
+            ATTACHMENT_NAME                 : 'Noms des piéces jointes',
+            OBSERVATIONS                    : 'Les observations',
+            PRIORITY_DEGREE                 : 'Dégré d\'urgence,',
+            PRIORITY_DEGREE_NORMAL          : 'Normal',
+            PRIORITY_DEGREE_URGENT          : 'Urgent',
+            PRIORITY_DEGREE_V_URGENT        : 'Trés urgent',
+            ATTACH_FILE                     : 'Importer un fichier ici'
+
         }
     }
 };
