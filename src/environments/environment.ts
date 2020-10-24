f8d616aa0e676fb6e530bddd30dab4fd6b631613
kaber <kabermoustapha@gmail.com>
2020-10-24 11:14:49 +0200

    set env ppd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     production: false,
     hmr       : false,
     // backendUrl: 'http://localhost:8080'
-    backendUrl: 'https://admin-courrier.herokuapp.com'
+    backendUrl: 'https://ppd-demat-courrier.herokuapp.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
