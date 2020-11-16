5f43d710756ceb906e81df62d819fbdbfdac5f47
elmokhtar18 <53345977+elmokhtar18@users.noreply.github.com>
2020-11-16 15:38:57 +0100

    remove the name of user in historique and add the ID of (direction/service) (#73)

--- src/app/models/applicationUser.ts
+++ src/app/models/applicationUser.ts
@@ -2,4 +2,7 @@ export class ApplicationUserModel{
     firstName: string;
     lastName: string;
     fonction: string;
+    codeDirection: string;
+    codeService: string;
+    codeDivision: string;
 }
