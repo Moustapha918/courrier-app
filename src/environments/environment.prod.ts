7efcbe3882512f343ff356f47454fb41bc58fab1
moustaphakaber <kabermoustapha@gmail.com>
2023-11-12 12:38:59 +0100

    branche heroku backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     hmr       : false,
     dev       : false,
-    backendUrl: 'https://ppd-demat-courrier.herokuapp.com'
+    backendUrl: 'https://desolate-bastion-03055-398588e863e9.herokuapp.com'
 };
